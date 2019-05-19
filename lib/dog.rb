1f848347522ee08370162c3c18ca5af30accda2f
Alan Krajina <alankrajina88@gmail.com>
2019-05-19 13:54:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  attr_accessor :name
+  
+  @@all=[]
+  
+  def initialize(name)
+    
+    @dog = name
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def name
+    @dog
+  end
+  
+  
+  def self.all
+    @@all.each do |doggy|
+      puts doggy
+    end  
+  end
+end
